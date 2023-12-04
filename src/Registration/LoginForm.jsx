4c54a93d04339ff6733cbd67d9be95769bd1ecc8
Mateusz Rosiński <mateusz.rosinski10@gmail.com>
2023-12-04 21:56:47 +0100

    Added Login and Registration component

---
+++ src/Registration/LoginForm.jsx
@@ -0,0 +1,94 @@
+// LoginForm.jsx
+import React, { useState } from "react";
+import {
+  Container,
+  Paper,
+  TextField,
+  Button,
+  Typography,
+  Grid,
+  InputAdornment,
+  IconButton,
+} from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+
+const LoginForm = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Tu możesz obsłużyć logikę logowania, np. przekazując dane do API
+    console.log("Dane logowania:", event.target);
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Paper
+        elevation={3}
+        style={{
+          marginTop: "30px",
+          padding: "20px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h5" gutterBottom>
+          Logowanie
+        </Typography>
+        <form
+          onSubmit={handleSubmit}
+          style={{ width: "100%", marginTop: "8px" }}
+        >
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="email"
+                label="Adres Email"
+                type="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="password"
+                label="Hasło"
+                type={showPassword ? "text" : "password"}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleTogglePassword}
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            style={{ marginTop: "20px" }}
+          >
+            Zaloguj
+          </Button>
+        </form>
+      </Paper>
+    </Container>
+  );
+};
+
+export default LoginForm;
